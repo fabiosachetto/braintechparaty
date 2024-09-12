13009ecf83e6aa1f6a8c5418ee2be2674953d7ea
Fábio Verdi Sachetto <fabio@sachetto.com.br>
2024-09-12 20:44:40 -0300

    Primeiro commit

---
+++ scripts/app.js
@@ -0,0 +1,55 @@
+function pesquisar() {
+    // Seleciona a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Obtém o valor digitado pelo usuário no campo de pesquisa
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+
+    // Expressão regular para validar se a entrada contém apenas letras
+    const regex = /^[a-zA-Z]+$/;
+
+    // Verifica se o campo de pesquisa está vazio
+    if (campoPesquisa.trim() === "") {
+        // Exibe uma mensagem de erro se o campo estiver vazio
+        section.innerHTML = "<p class='alerta'>Vc não digitou nada!!!</p>";
+        return;
+    } else if (!regex.test(campoPesquisa)) {
+        // Exibe uma mensagem de erro se a entrada contiver caracteres inválidos
+        section.innerHTML = "<p class='alerta'>Digite apenas letras.</p>";
+        return;
+    } else {
+        // Itera sobre cada dado na lista de dados
+        for (let dado of dados) {
+            // Função auxiliar para remover acentos de uma string
+            function removerAcentos(str) {
+                return str.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+            };
+
+            // Verifica se o título do dado começa com a letra pesquisada (ignorando acentos e case-sensitive)
+            if (removerAcentos(dado.titulo.toLowerCase()).startsWith(removerAcentos(campoPesquisa.toLowerCase()))) {
+                // Adiciona um novo elemento HTML com os detalhes do dado aos resultados
+                resultados += `
+                <div class="item-resultado">
+                    <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                    </h2>
+                    <p class="descricao-meta">Especialidades: ${dado.descricao}</p>
+                    <p class="descricao-meta">Tempo de Profissão: ${dado.temponapista}</p>
+                    <p class="descricao-meta">Idade: ${dado.idade}</p>
+                </div>
+                `; 
+            };
+        };
+
+        if (!resultados) {
+            section.innerHTML = "<p class='alerta'>O que você digitou não foi encontrado!!!</p>";
+            return;
+        };
+
+        // Exibe os resultados da pesquisa na seção HTML
+        section.innerHTML = resultados;
+    };
+}
---
+++ scripts/dados.js
@@ -0,0 +1,29 @@
+let dados = [
+    {
+        titulo : "Fabão",
+        descricao : "Motoboy, Programador Front-End, Marceneiro, Serralheiro.",
+        temponapista: "5 anos em Paraty",
+        idade : 45,
+    },
+
+    {
+        titulo : "Rodrigão Fiel Paraty",
+        descricao : "Motoboy, Bala de Infra em T.I, gambá e turista nato.",
+        temponapista: "7 anos em Paraty",
+        idade : 40,
+    },
+
+    {
+        titulo : "Fabuloso",
+        descricao : "Motoboy, Garçom e Capoeirista.",
+        temponapista: "10 anos em Paraty",
+        idade : 30,
+    },
+
+    {
+        titulo : "Tattiana",
+        descricao : "Camareira, professora, empreendedora do Fome de Batata.",
+        temponapista: "5 anos em Paraty",
+        idade : 44,
+    },
+];
