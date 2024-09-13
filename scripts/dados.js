33f2d2c24557fd7f34b775e5ce18dae3be15eac8
Fábio Verdi Sachetto <fabio@sachetto.com.br>
2024-09-13 17:22:14 -0300

    Update dados.js
    adicionando integrantes do grupo

--- scripts/dados.js
+++ scripts/dados.js
@@ -1,29 +1,177 @@
 let dados = [
     {
-        titulo : "Fabão",
+        titulo : "Fábio Verdi Sachetto",
         descricao : "Motoboy, Programador Front-End, Marceneiro, Serralheiro.",
         temponapista: "5 anos em Paraty",
         idade : 45,
+        celular: "https://wa.me/+5524992815187"
     },
 
     {
-        titulo : "Rodrigão Fiel Paraty",
-        descricao : "Motoboy, Bala de Infra em T.I, gambá e turista nato.",
+        titulo : "Rodrigo",
+        descricao : "Motoboy, Bala de Infra em T.I.",
         temponapista: "7 anos em Paraty",
         idade : 40,
+        celular: "https://wa.me/+5524992815187"
     },
 
     {
-        titulo : "Fabuloso",
+        titulo : "Fabulo",
         descricao : "Motoboy, Garçom e Capoeirista.",
         temponapista: "10 anos em Paraty",
         idade : 30,
+        celular: "https://wa.me/+5524992815187"
     },
 
     {
-        titulo : "Tattiana",
-        descricao : "Camareira, professora, empreendedora do Fome de Batata.",
+        titulo : "André - Capacete na Testa",
+        descricao : "Motoboy, jogador do Cirrose FC",
         temponapista: "5 anos em Paraty",
-        idade : 44,
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Cauê Casa Pizza",
+        descricao : "Pizzaiolo, Empreendedor, Marinheiro",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Alan Palmeiras",
+        descricao : "Motoboy, Empreendedor e ex-cachaceiro",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Alexandre Diniz",
+        descricao : "Motoboy e Bicicleteiro",
+        temponapista: "5 anos em Paraty",
+        idade : 18,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Alice (Dona do Ezequiel",
+        descricao : "Motogirl e Farmaceutica",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "André da Crosser",
+        descricao : "Motoboy e Ator na Família Addams",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Cesar",
+        descricao : "Motoboy",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Davi",
+        descricao : "Motoboy, Empreendedor e Paizão",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Davizinho",
+        descricao : "Motoboy, Muleke Doido e Vendedor de Terreno.",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Gilson",
+        descricao : "Motoboy.",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Jonathan",
+        descricao : "Motoboy",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Lucas",
+        descricao : "Motoboy e Mecânico.",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Marco ou Marcos Aurélio",
+        descricao : "Motoboy e Gerente do Mercado doKekeu",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Moisés",
+        descricao : "Motoboy",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Portugal",
+        descricao : "Motoboy",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Rafael - Esfiharia Pacheco",
+        descricao : "Motoboy e Empreendedor",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Rafael Vila",
+        descricao : "Motoboy",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Rick",
+        descricao : "Motoboy, Dono do Ogami Sushi e Lutador de Muay Thay",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
+    },
+
+    {
+        titulo : "Sergio",
+        descricao : "Motoboy e Empreendedor do Yakisoba Paraty",
+        temponapista: "5 anos em Paraty",
+        idade : 28,
+        celular: "https://wa.me/+5524992815187"
     },
 ];
