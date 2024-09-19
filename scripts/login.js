91c4e8c1f12e5be05a3ddf112e0b03156834f5f2
Fábio Verdi Sachetto <fabio@sachetto.com.br>
2024-09-19 20:42:03 -0300

    Update login.js
    mostrando o nome do usuario apos fazer o login

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,8 @@
 const formLogin = document.getElementById("form-login");
 
+// Obtém o elemento span e atualiza o conteúdo
+const nomeUsuarioExibido = document.getElementById("nome-usuario-exibido");
+
 formLogin.addEventListener("submit", (event) => {
     event.preventDefault(); // Impede o envio padrão do formulário
 
@@ -9,28 +12,32 @@ formLogin.addEventListener("submit", (event) => {
     // Simulando um banco de dados (substitua por sua lógica de verificação)
     const usuarios = [
         { 
-            email: "fabio@teste.com.br", senha: "palmeiras"
+            email: "fabio@teste.com.br", senha: "123", nomeUsuarioExibido: "Fábio Verdi"
         },
         { 
-            email: "rodrigo@teste.com.br", senha: "corinthians"
+            email: "rodrigo@teste.com.br", senha: "corinthians", nomeUsuarioExibido: "Rodrigo"
         },
         { 
-            email: "caue@teste.com.br", senha: "casapizza"
+            email: "caue@teste.com.br", senha: "casapizza", nomeUsuarioExibido: "Cauê"
         }
     ];
 
-    let usuarioEncontrado = false;
+    let nomeUsuarioEncontrado = null;
+
     usuarios.forEach(usuario => {
         if (usuario.email === email && usuario.senha === senha) {
-            usuarioEncontrado = true;
+            nomeUsuarioEncontrado = usuario.nomeUsuarioExibido;
         }
     });
 
-    if (usuarioEncontrado) {
-        // Redireciona para a página restrita
+    if (nomeUsuarioEncontrado) {
+        // Armazena o nome do usuário no localStorage
+        localStorage.setItem("nomeUsuario", nomeUsuarioEncontrado);
+
+        // Redireciona para a página principal
         window.location.href = "logado.html";
     } else {
         // Exibe uma mensagem de erro
         alert("Email ou senha inválidos.");
-    }
+    };
 });
