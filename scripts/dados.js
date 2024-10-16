d510bd496981971992c6f66c73a6018f980a6f4c
Fábio Verdi Sachetto <fabio@sachetto.com.br>
2024-10-16 15:33:41 -0300

    Update dados.js
    adicionando colaborador no banco fake

--- scripts/dados.js
+++ scripts/dados.js
@@ -1,7 +1,7 @@
 let dados = [
     {
         titulo : "Fábio Verdi Sachetto",
-        descricao : "Motoboy - Programador Front-End - Marceneiro - Serralheiro - Manutenção e Instalação de Ar-Condicionado e Empreendedor do Delivery Fome de Batata em Paraty",
+        descricao : "Motoboy - Programador Front-End - Marceneiro - Serralheiro - Manutenção e Instalação de Ar-Condicionado e Empreendedor do Delivery Fome de Batata em Paraty.",
         temponapista: "5 anos em Paraty",
         idade : 45,
         celular: "https://wa.me/+5524000000000"
@@ -33,7 +33,7 @@ let dados = [
 
     {
         titulo : "Rick",
-        descricao : "Motoboy - Empreendero do Ogami Sushi e Lutador de Muay Thay.",
+        descricao : "Motoboy - Empreendedor do Ogami Sushi - Lutador de Muay Thay.",
         temponapista: "?",
         idade : "?",
         celular: "https://wa.me/+5524000000000"
@@ -42,6 +42,14 @@ let dados = [
     {
         titulo : "Serginho",
         descricao : "Motoboy - Empreendedor do Yakissoba Paraty - Garçom e Gerente de Restaurante.",
+        temponapista: "?",
+        idade : "?",
+        celular: "https://wa.me/+5524000000000"
+    },
+
+    {
+        titulo : "Igão",
+        descricao : "Motoboy - Empreendedor do Rei da Chapa Burguer - Garçom - Taxista.",
         temponapista: "?",
         idade : "?",
         celular: "https://wa.me/+5524000000000"
